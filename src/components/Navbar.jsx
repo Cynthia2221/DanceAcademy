f6963ab337b80f4a3645401298359dba6b57bdd8
Cynthia <cynthiasanchezher@gmail.com>
2024-04-30 00:54:47 +0100

    Correcciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, {useState}from 'react'
-import {Link, NavLink} from "react-router-dom";
+import React, { useState } from 'react'
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/header/logo.png"
 
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (<nav>
     <Link to="/" className="title"><p><img src={logo} alt=""></img></p></Link>
-    <div className="menu" onClick={() =>{
+    <div className="menu" onClick={() => {
       setMenuOpen(!menuOpen);
     }}>
       <span></span>
@@ -16,9 +16,9 @@ export const Navbar = () => {
       <span></span>
     </div>
     <ul className={menuOpen ? "open" : ""}>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-        <li><NavLink to="/services">Services</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
+      <li><NavLink to="/services">Services</NavLink></li>
     </ul>
   </nav>
   )
