f01aba5f8e117a69463216f1024c0b4648606d74
Cynthia <cynthiasanchezher@gmail.com>
2024-04-30 15:28:04 +0100

    Cambios para la segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       {<Navbar /> }
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
